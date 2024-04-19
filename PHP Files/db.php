8a5615247962d66caae27d742692d6faaef4fcf6
SusithD <iamsusithalwis@gmail.com>
2024-04-19 22:00:05 +0530

    Updated the db.php file with a sample Database Name

--- PHP Files/db.php
+++ PHP Files/db.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "amandi";
+$dbname = "yourDBname";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
